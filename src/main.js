30b60cf4e5686a7cdb13cb9662c6a15c50b041cb
yangxiaoping <2878847069@qq.com>
2020-12-27 17:42:45 +0800

    feat: css压缩

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './main.css'
 
 const p  = new Promise((resolve, reject) => {
   resolve('123')
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,31 @@
 const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 const prodConfig = {
-  mode: 'development',
+  mode: 'production',
   plugins: [
-    new CleanWebpackPlugin()
-  ]
+    new CleanWebpackPlugin(),
+    // new OptimizeCSSAssetsPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial' // 只打包初始时依赖的第三方
+        }
+      }
+    },
+    runtimeChunk: 'multiple',
+    minimizer: [
+      new OptimizeCSSAssetsPlugin()
+    ]
+  }
 }
 
 module.exports = merge(commonConfig, prodConfig)
